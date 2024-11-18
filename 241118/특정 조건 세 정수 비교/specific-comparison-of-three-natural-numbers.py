8bed35400edc304518cbd5496c3f7f469e63d55c
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2024-11-18 23:58:16 +0900

    Title: 특정 조건 세 정수 비교, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 241118/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 241118/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -1,10 +1,10 @@
 a,b,c=map(int,input().split())
 
-if a<b and a<c:
+if a<=b and a<=c:
     print("1", end=" ")
 else:
     print("0", end=" ")
-if a==b==c:
+if a==b and b==c and a==c:
     print("1")
 else:
     print("0")
