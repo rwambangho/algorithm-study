1f801ba071a68dbe0d3a5f6ea78673f6d6ae0866
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2024-12-02 22:19:01 +0900

    Title: 숫자의 계절은, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241202/숫자의 계절은/season-of-num.py
@@ -0,0 +1,9 @@
+a=int(input())
+if a == 3 or a ==4 or a == 5:
+    print('Spring')
+if a == 6 or a == 7 or a == 8:
+    print('Summer')
+if a == 9 or a == 10 or a == 11:
+    print('Fall')
+if a == 12 or a ==1 or a == 2:
+    print('Winter')
