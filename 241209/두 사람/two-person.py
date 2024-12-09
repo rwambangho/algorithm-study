46c0096c10e6c150f45f57960e1b02609de6d3a1
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2024-12-09 23:46:12 +0900

    Title: 두 사람, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241209/두 사람/two-person.py
+++ 241209/두 사람/two-person.py
@@ -2,7 +2,7 @@ age,sex=input().split()
 age=int(age)
 age2,sex2=input().split()
 age2=int(age2)
-if (age,age2 >= 19 and sex,sex2 == 'M'):
+if (age >= 19 and sex == 'M') or (age2 >= 19 and sex2 == 'M'):
     print('1')
 else:
     print('0')
