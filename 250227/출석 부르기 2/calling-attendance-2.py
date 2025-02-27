158c185984cf5b6999f7da9533f7a62bfeef236f
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-02-27 15:57:57 +0900

    Title: 출석 부르기 2, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250227/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,14 @@
+for _ in range(100):
+    n=int(input())
+    if n==1:
+        print("John")
+    elif n==2:
+        print("Tom")
+    elif n==3:
+        print("Paul")
+    elif n==4:
+        print("Sam")
+    else:
+        print("Vacancy")
+        break
+    
