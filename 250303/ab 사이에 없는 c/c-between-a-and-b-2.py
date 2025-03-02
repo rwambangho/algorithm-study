e37130cfa9fba9ce767d9be9ce90c373449ed78b
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-03-03 00:00:45 +0900

    Title: ab 사이에 없는 c, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250303/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -0,0 +1,8 @@
+a, b, c = map(int, input().split())
+
+for i in range(a, b + 1):
+    if i % c == 0:  # 첫 번째 배수를 찾으면 바로 YES 출력 후 종료
+        print("NO")
+        break
+else:  # for 루프가 break 없이 끝나면 실행됨
+    print("YES")
