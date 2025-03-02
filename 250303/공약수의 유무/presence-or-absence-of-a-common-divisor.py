bb8f00bf07d0a8033f3651c7dc7bce1b1f39980c
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-03-03 01:14:41 +0900

    Title: 공약수의 유무, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250303/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -0,0 +1,12 @@
+a,b=map(int,input().split())
+sat=False
+for i in range(a,b+1):
+    if 1920 % i == 0 and 2880 % i == 0:
+        sat=True
+        break
+    
+if sat==True:
+    print(1)
+else:
+    print(0)
+    
