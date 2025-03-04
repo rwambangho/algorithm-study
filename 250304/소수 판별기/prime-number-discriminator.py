3c0c86cbc1aec3f040a642b1ab619db7c26c15db
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-03-04 23:52:38 +0900

    Title: 소수 판별기, Time: 50ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250304/소수 판별기/prime-number-discriminator.py
@@ -0,0 +1,13 @@
+n=int(input())
+sat=True
+for i in range(2,n):
+    if n % i == 0:
+        sat=False
+        break
+    else:
+        continue
+        
+if sat==True:
+    print("P")
+else:
+    print("C")
