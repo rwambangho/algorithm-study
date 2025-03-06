27b73d1f0a69d024f62ffa1752b84802a870029c
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-03-06 17:23:54 +0900

    Title: 두 개의 직각삼각형, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250306/두 개의 직각삼각형/two-right-triangle.py
@@ -0,0 +1,11 @@
+n=int(input())
+for i in range(n):
+    for j in range(n-i):
+        print("*",end='')
+
+    for j in range(i*2):
+        print(" ",end='')
+    
+    for j in range(n-i):
+        print("*",end='')
+    print()
