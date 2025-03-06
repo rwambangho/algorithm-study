ae3f02fd753e65971924038aed2e5b3977e94d98
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-03-06 17:21:58 +0900

    Title: 별표 출력하기 4, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250306/별표 출력하기 4/print-star-4.py
@@ -0,0 +1,10 @@
+n=int(input())
+for i in range(n):
+    for j in range(n-i):
+        print("*",end=' ')
+    print()
+
+for i in range(1,n):
+    for j in range(i+1):
+        print("*",end=' ')
+    print()
