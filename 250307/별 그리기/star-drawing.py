c11bff8baf740096442725bdc871a48136579410
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-03-07 17:49:34 +0900

    Title: 별 그리기, Time: 71ms, Memory: 18MB, Status: Passed - CodeTree

--- 250307/별 그리기/star-drawing.py
+++ 250307/별 그리기/star-drawing.py
@@ -11,9 +11,9 @@ for i in range(n):
     print()
 
 for i in range(n-1):
-    for j in range(n+i-2):
+    for j in range(i+1):
         print(" ",end='')
     
-    for j in range(n-i*2):
+    for j in range((n*2)-(3+i*2)):
         print("*",end='')
     print()
