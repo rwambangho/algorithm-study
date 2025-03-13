0c280e639d9921ed7c87bbe6e3f51f303b94ef52
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-03-13 23:26:24 +0900

    Title: 최소공배수 구하기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250313/최소공배수 구하기/find-the-least-common-multiple.py
+++ 250313/최소공배수 구하기/find-the-least-common-multiple.py
@@ -3,7 +3,7 @@ n, m = map(int, input().split())
 # Please write your code here.
 def f(n,m):
 
-    for i in range(1,n*m):
+    for i in range(1,n*m+1):
         if i % n ==0 and i % m == 0:
             print(i)
             break
