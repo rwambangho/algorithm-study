ee92d850e1d25a3fb0ae715fb33ac9d001ec8b25
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-03-20 23:57:10 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250320/원소 10개의 합/sum-of-10-elements.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n[10];
+    int sum=0;
+    for(int i=0; i<10; i++){
+        cin >> n[i];
+        sum+=n[i];
+    
+    }
+       
+    cout << sum;
+    
+    // Please write your code here.
+    return 0;
+}
