d2b2a7f6e9540a6d5e00b7bc0447082c637a18f1
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-03-26 14:49:01 +0900

    Title: 특정 위치의 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250326/특정 위치의 수/numbers-in-specific-location.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    for(int i=0; i<10; i++){
+        cin >> arr[i];
+    }
+    int total = arr[2]+arr[4]+arr[9];
+    cout << total;
+
+    // Please write your code here.
+    return 0;
+}
