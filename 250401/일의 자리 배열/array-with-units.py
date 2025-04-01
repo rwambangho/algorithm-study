34196713d6193677c27f721dc0842f90fe8330b3
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-04-01 17:15:19 +0900

    Title: 일의 자리 배열, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250401/일의 자리 배열/array-with-units.py
+++ 250401/일의 자리 배열/array-with-units.py
@@ -1,9 +1,7 @@
-n,m = map(int,input().split())
+n,m = tuple(map(int,input().split()))
 arr=list()
 arr.append(n)
 arr.append(m)
-for _ in range(4):
-    n, m= n+m, n+m+m
-    arr.append(n%10)
-    arr.append(m%10)
+for i in range(2, 10):
+    arr.append((arr[i-2]+arr[i-1])%10)
 print(*arr)
