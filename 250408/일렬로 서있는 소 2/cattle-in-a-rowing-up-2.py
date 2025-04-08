d367abe82bf91e87f6871e24b7628a2aaa02a23d
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-04-08 23:58:51 +0900

    Title: 일렬로 서있는 소 2, Time: 53ms, Memory: 17MB, Status: Passed - Codetree

--- 250408/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 250408/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -4,8 +4,13 @@ total=0
 for i in range(n):
     for j in range(i+1,n):
         for k in range(j+1,n):
-            if A[i] < A[j] < A[k]:
+            if A[i] <= A[j] <= A[k]:
                 total+=1
+
+            
+
+
+
 print(total)
             
             
