936c01a64d3d0918026233d9682506cf11a3ef27
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-04-09 23:50:33 +0900

    Title: 최고의 13위치, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250409/최고의 13위치/best-place-of-13.py
+++ 250409/최고의 13위치/best-place-of-13.py
@@ -3,7 +3,7 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 cnt=0
 for i in range(n):
     for j in range(n-2):
-        cnt=max(cnt,grid[i][j]+grid[i][j+2])
+        cnt=max(cnt,grid[i][j]+grid[i][j+1]+grid[i][j+2])
 print(cnt)
 
 
