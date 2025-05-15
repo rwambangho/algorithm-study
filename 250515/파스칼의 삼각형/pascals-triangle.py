f0476f83b847e80c497f1477c84d096ad00f1785
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-05-15 23:55:21 +0900

    Title: 파스칼의 삼각형, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250515/파스칼의 삼각형/pascals-triangle.py
+++ 250515/파스칼의 삼각형/pascals-triangle.py
@@ -3,13 +3,11 @@ grid=[[0 for _ in range(n)]for _ in range(n)]
 
 for i in range(n):
     grid[i][0]=1
-    for j in range(1,n):
-        if grid[i-1][j-1] in grid and grid[i-1][j] in grid: 
-            grid[i][j]=grid[i-1][j-1]+grid[i-1][j]
-        else:
-            grid[i][j]=1
+    for j in range(i):
+        grid[i][j+1]=grid[i-1][j]+grid[i-1][j+1]
 
 for row in grid:
     for elem in row:
-        print(elem,end=' ')
+        if elem != 0:
+            print(elem,end=' ')
     print()
