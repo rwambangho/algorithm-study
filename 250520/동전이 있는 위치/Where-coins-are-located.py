700f9254624e606cb5b4c52d96c9483ac371d253
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-05-20 23:49:33 +0900

    Title: 동전이 있는 위치, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250520/동전이 있는 위치/Where-coins-are-located.py
@@ -0,0 +1,19 @@
+n,m=map(int,input().split())
+arr=[[0 for _ in range(n)]for _ in range(n)]
+for _ in range(m):
+    r,c=tuple(map(int,input().split()))
+    arr[r-1][c-1]=1
+
+for row in arr:
+    for elem in row:
+        print(elem,end=' ')
+    print()
+
+   
+
+
+            
+
+
+
+
