f6b1b15752627fa80ca4e621b614ce8733ef1edf
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-06-24 23:47:24 +0900

    Title: 좌우로 반복하며 출력, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250624/좌우로 반복하며 출력/print-in-left-right-reverse.py
@@ -0,0 +1,9 @@
+n=int(input())
+for i in range(n):
+    if i%2==0:
+        for j in range(n):
+            print(j+1,end='')
+    else:
+        for j in range(n):
+            print(n-j,end='')
+    print()
