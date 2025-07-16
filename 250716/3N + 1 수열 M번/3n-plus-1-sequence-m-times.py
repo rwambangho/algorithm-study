1e08ff52b0d0b407e1575ebbc9a0547b74fdb087
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-07-16 23:58:47 +0900

    Title: 3N + 1 수열 M번, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250716/3N + 1 수열 M번/3n-plus-1-sequence-m-times.py
@@ -0,0 +1,14 @@
+m=int(input())
+for _ in range(m):
+    cnt=0
+    n=int(input())
+
+    while n!=1:
+        if n%2==0:
+            n=n//2
+        else:
+            n=n*3+1
+        cnt+=1
+
+print(cnt)
+
