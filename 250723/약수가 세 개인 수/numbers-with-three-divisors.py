da133eae0ec7ad7335b8328c613a95dfa33726d1
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-07-23 23:20:18 +0900

    Title: 약수가 세 개인 수, Time: 51ms, Memory: 17MB, Status: Passed - Codetree

--- 250723/약수가 세 개인 수/numbers-with-three-divisors.py
+++ 250723/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -1,7 +1,7 @@
 start, end = map(int, input().split())
 cnt=0
 # Please write your code here.
-for i in range(start,end):
+for i in range(start,end+1):
     tmp=0
     for j in range(1,i+1):
         if i%j==0:
