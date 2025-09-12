74a423aa105972bf535e7e8d7912b60525d0342f
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-09-12 17:51:05 +0900

    [level 1] Title: 모의고사, Time: 9.13 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,43 +1,20 @@
 def solution(answers):
-    answer = []
-    a=len(answers)
-    cnt1, cnt2, cnt3 = 0, 0, 0
-    n1=[[1,2,3,4,5] for _ in range(a)]
-    n2=[[ 2, 1, 2, 3, 2, 4, 2, 5]for _ in range(a)]
-    n3=[[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]for _ in range(a)]
-    s1=[item for row in n1 for item in row]
-    s2=[item for row in n2 for item in row]
-    s3=[item for row in n3 for item in row]
+    patterns=[[ 1, 2, 3, 4, 5],[2, 1, 2, 3, 2, 4, 2, 5],[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    scores=[0]*3
     
+    for i,answer in enumerate(answers):
+        for j,pattern in enumerate(patterns):
+            if answer==pattern[i%len(pattern)]:
+                scores[j]+=1
+    max_score=max(scores)
+    high_score=[]
+    for i,score in enumerate(scores):
+        if score==max_score:
+            high_score.append(i+1)
+            
+            
     
-    for i in range(a):
-        if s1[i] == answers[i]:
-            cnt1+=1
-        if s2[i]==answers[i]:
-            cnt2+=1
-        if s3[i]==answers[i]:
-            cnt3+=1
+                
+                
     
-    
-    if cnt1 > cnt2 and cnt1 > cnt3: #1
-        answer.append(1)
-    elif cnt2 > cnt1 and cnt2 > cnt3: #2
-        answer.append(2)
-    elif cnt3 > cnt1 and cnt3 > cnt2: #4
-        answer.append(3)
-    elif cnt1 > cnt3 and cnt2 > cnt3 and cnt1 == cnt2: #1 2
-        answer.append(1)
-        answer.append(2)
-    elif cnt1 > cnt2 and cnt3 > cnt2 and cnt1 == cnt3: # 1 3
-        answer.append(1)
-        answer.append(3)
-    elif cnt2 > cnt1 and cnt3 > cnt1 and cnt2 == cnt3: # 2 3
-        answer.append(2)
-        answer.append(3)
-    elif cnt1 == cnt2 == cnt3: # 1 2 3
-        answer.append(1)
-        answer.append(2)
-        answer.append(3)
-        
-              
-    return answer
+    return high_score
