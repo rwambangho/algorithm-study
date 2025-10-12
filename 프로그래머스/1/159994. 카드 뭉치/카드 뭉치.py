c138c89f87c9d1825b430de24d5f2828f2566f18
Byeong Ho Yoon <121777977+rwambangho@users.noreply.github.com>
2025-10-12 16:55:39 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,20 +1,35 @@
 from collections import deque
-
 def solution(cards1, cards2, goal):
-  # cards와 goal을 deque로 변환
-  cards1 = deque(cards1)
-  cards2 = deque(cards2)
-  goal = deque(goal)
-
-  # ➊ goal의 문자열을 순차적으로 순회
-  while goal:
-    if cards1 and cards1[0] == goal[0]:  # ➋ card1의 front와 일치하는 경우
-      cards1.popleft()
-      goal.popleft()
-    elif cards2 and cards2[0] == goal[0]:  # ➌ card2의 front와 일치하는 경우
-      cards2.popleft()
-      goal.popleft()
-    else:
-      break  # 일치하는 원소를 찾지 못했으므로 종료
-
-  return "Yes" if not goal else "No"  # ➍ goal이 비었으면 “Yes” 아니면 “No”를 반환
+    answer = 'Yes'
+    goal=deque(goal)
+    cards1=deque(cards1)
+    cards2=deque(cards2)
+    
+    
+    # goal을 탐색 
+    # cards1과 cards2의 popleft에 goal의 첫번째 단어가 있는지 확인
+    # 
+    # 둘 다 없다면 answer에 No를 반환
+    while goal:
+        
+        if cards1[0]==goal[0]:
+            goal.popleft()
+            c1=cards1.popleft()
+            cards1.append(c1)
+        elif cards2[0]==goal[0]:
+            goal.popleft()
+            c2=cards2.popleft()
+            cards2.append(c2)
+        else:
+            answer='No'
+            break
+            
+        
+        
+        
+    
+        
+        
+    
+        
+    return answer
